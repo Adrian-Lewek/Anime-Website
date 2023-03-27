beef6303cb0a9581b65f15d65d4ec496acb4a406
Adrian-Lewek <adrian.lewek24@gmail.com>
2023-03-27 16:25:33 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './style/index.scss';
 import App from './pages/App';
 import {store} from './redux/store'
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import {  HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter basename='/Anime-Website'>
+    <HashRouter basename='/Anime-Website'>
       <App />
-    </BrowserRouter>
+    </HashRouter>
     </Provider>
   </React.StrictMode>
 );
